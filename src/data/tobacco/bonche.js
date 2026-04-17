9575a6e1cccee904830ac6249a3c41fa0a72900f
MaxbIch <shadow.maximka@gmail.com>
2026-04-17 18:23:24 +0700

    add checkout form

--- src/data/tobacco/bonche.js
+++ src/data/tobacco/bonche.js
@@ -1,17 +1,122 @@
+import image1 from "../../img/bonche/barberry.png";
+import image2 from "../../img/bonche/blackcurrant.png";
+import image3 from "../../img/bonche/caramel.png";
+import image4 from "../../img/bonche/cheesecake.png";
+import image5 from "../../img/bonche/cherry.png";
+import image6 from "../../img/bonche/coffee.png";
+import image7 from "../../img/bonche/cognac.png";
+import image8 from "../../img/bonche/darkchocolate.png";
+import image9 from "../../img/bonche/grapefruit.png";
+import image10 from "../../img/bonche/lychee.png";
+import image11 from "../../img/bonche/mango.png";
+import image12 from "../../img/bonche/melisa.png";
+import image13 from "../../img/bonche/raspberry.png";
+
 const bonche = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Barberry",
+        taste: "Барбарис",
+        price: '1.000.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
+        name: "Black currant",
+        taste: "Черная смородина",
+        price: '1.000.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Caramel",
+        taste: "Карамель",
+        price: '1.000.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 4,
+        name: "Cheesecake",
+        taste: "Чизкейк",
+        price: '1.000.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Cherry",
+        taste: "Вишня",
+        price: '1.000.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
+    },{
+        id: 6,
+        name: "Coffee",
+        taste: "Кофе",
+        price: '1.000.000',
+        previewImage: image6,
+        fullImage: image6,
+        description: ""
+    },{
+        id: 7,
+        name: "Cognac",
+        taste: "Коньяк",
+        price: '1.000.000',
+        previewImage: image7,
+        fullImage: image7,
+        description: ""
+    },{
+        id: 8,
+        name: "Dark chocolate",
+        taste: "Темный шоколад",
+        price: '1.000.000',
+        previewImage: image8,
+        fullImage: image8,
+        description: ""
+    },{
+        id: 9,
+        name: "Grapefruit",
+        taste: "Грейпфрут",
+        price: '1.000.000',
+        previewImage: image9,
+        fullImage: image9,
+        description: ""
+    },{
+        id: 10,
+        name: "Lychee",
+        taste: "Личи",
+        price: '1.000.000',
+        previewImage: image10,
+        fullImage: image10,
+        description: ""
+    },{
+        id: 11,
+        name: "Mango",
+        taste: "Манго",
+        price: '1.000.000',
+        previewImage: image11,
+        fullImage: image11,
+        description: ""
+    },{
+        id: 12,
+        name: "Melisa",
+        taste: "Мелиса",
+        price: '1.000.000',
+        previewImage: image12,
+        fullImage: image12,
+        description: ""
+    },{
+        id: 13,
+        name: "Raspberry",
+        taste: "Малина",
+        price: '1.000.000',
+        previewImage: image13,
+        fullImage: image13,
+        description: ""
     }
 ];
 
