a2871078c1229d59af721f43e0d060519bbf003d
MaxbIch <shadow.maximka@gmail.com>
2026-04-17 17:37:08 +0700

    add checkout form

--- src/data/tobacco/darkside.js
+++ src/data/tobacco/darkside.js
@@ -1,18 +1,170 @@
+import image1 from "../../img/darkside/barvyor.png";
+import image2 from "../../img/darkside/basp.png";
+import image3 from "../../img/darkside/bitr.png";
+import image4 from "../../img/darkside/bounty.png";
+import image5 from "../../img/darkside/breakingred.png";
+import image6 from "../../img/darkside/cola.png";
+import image7 from "../../img/darkside/cosmoflower.png";
+import image8 from "../../img/darkside/darkpas.png";
+import image9 from "../../img/darkside/grap.png";
+import image10 from "../../img/darkside/grapecore.png";
+import image11 from "../../img/darkside/icegranny.png";
+import image12 from "../../img/darkside/mango.png";
+import image13 from "../../img/darkside/needls.png";
+import image14 from "../../img/darkside/pomelow.png";
+import image15 from "../../img/darkside/raspberry.png";
+import image16 from "../../img/darkside/supernova.png";
+import image17 from "../../img/darkside/torpedo.png";
+import image18 from "../../img/darkside/tropicray.png";
+
+
 const darkside = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Barvy orange",
+        taste: "Апельсин",
+        price: '550.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
+        name: "Bassberry",
+        taste: "Бузина",
+        price: '550.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Bittersweet",
+        taste: "Тоник + лимон",
+        price: '550.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 4,
+        name: "Bounty Hunter",
+        taste: "Кокос",
+        price: '550.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Breaking Red",
+        taste: " Гранат",
+        price: '550.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
+    },{
+        id: 6,
+        name: "Darkside Cola",
+        taste: "Кола",
+        price: '550.000',
+        previewImage: image6,
+        fullImage: image6,
+        description: ""
+    },{
+        id: 7,
+        name: "Cosmo Flower",
+        taste: "Черника, сирень",
+        price: '550.000',
+        previewImage: image7,
+        fullImage: image7,
+        description: ""
+    },{
+        id: 8,
+        name: "Dark Passion",
+        taste: "Маракуйя",
+        price: '550.000',
+        previewImage: image8,
+        fullImage: image8,
+        description: ""
+    },{
+        id: 9,
+        name: "Kalee Grapefruit",
+        taste: "Грейпфрут",
+        price: '550.000',
+        previewImage: image9,
+        fullImage: image9,
+        description: ""
+    },{
+        id: 10,
+        name: "Grape Core",
+        taste: "Виноград",
+        price: '550.000',
+        previewImage: image10,
+        fullImage: image10,
+        description: ""
+    },{
+        id: 11,
+        name: "Ice Granny",
+        taste: "Яблоко",
+        price: '550.000',
+        previewImage: image11,
+        fullImage: image11,
+        description: ""
+    },{
+        id: 12,
+        name: "Mango Lassi",
+        taste: "Манго",
+        price: '550.000',
+        previewImage: image12,
+        fullImage: image12,
+        description: ""
+    },{
+        id: 13,
+        name: "Needles",
+        taste: "Хвоя",
+        price: '550.000',
+        previewImage: image13,
+        fullImage: image13,
+        description: ""
+    },{
+        id: 14,
+        name: "Pomelow",
+        taste: "Помело",
+        price: '550.000',
+        previewImage: image14,
+        fullImage: image14,
+        description: ""
+    },{
+        id: 15,
+        name: "Raspberry",
+        taste: "Малина",
+        price: '550.000',
+        previewImage: image15,
+        fullImage: image15,
+        description: ""
+    },{
+        id: 16,
+        name: "Supernova",
+        taste: "Холод",
+        price: '550.000',
+        previewImage: image16,
+        fullImage: image16,
+        description: ""
+    },{
+        id: 17,
+        name: "Torpedo",
+        taste: "Арбуз + Дыня",
+        price: '550.000',
+        previewImage: image17,
+        fullImage: image17,
+        description: ""
+    },{
+        id: 18,
+        name: "Tropic Ray",
+        taste: "Карибский коктейль",
+        price: '550.000',
+        previewImage: image18,
+        fullImage: image18,
+        description: ""
     }
+
 ];
 
 export default darkside;
