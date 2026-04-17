2e59e4b81df9c66e116da8aff20b186f028d9c92
MaxbIch <shadow.maximka@gmail.com>
2026-04-17 19:15:41 +0700

    add checkout form

--- src/data/tobacco/satyr.js
+++ src/data/tobacco/satyr.js
@@ -1,18 +1,142 @@
+import image1 from "../../img/satyr/ananas.jpeg";
+import image2 from "../../img/satyr/angels.png";
+import image3 from "../../img/satyr/bom_byao.png";
+import image4 from "../../img/satyr/burley.png";
+import image5 from "../../img/satyr/dedushka.png";
+import image6 from "../../img/satyr/energy.png";
+import image7 from "../../img/satyr/gogo.png";
+import image8 from "../../img/satyr/good_lemon.png";
+import image9 from "../../img/satyr/grap.png";
+import image10 from "../../img/satyr/jager.png";
+import image11 from "../../img/satyr/lastochka.png";
+import image12 from "../../img/satyr/nezhnost.png";
+import image13 from "../../img/satyr/queen.png";
+import image14 from "../../img/satyr/tochkag.png";
+import image15 from "../../img/satyr/turbo.jpeg";
+
+
 const satyr = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Ananas",
+        taste: "Ананас",
+        price: '900.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
-    }
+        name: "Angels tits",
+        taste: "Коктейльная вишня",
+        price: '900.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Bom Byao",
+        taste: "Виноградная газировка",
+        price: '900.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 4,
+        name: "Burley cointreau",
+        taste: "Берли на цитрусах",
+        price: '900.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Dedushka",
+        taste: "Лесная земляника",
+        price: '900.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
+    },{
+        id: 6,
+        name: "Energy",
+        taste: "Энергетик",
+        price: '900.000',
+        previewImage: image6,
+        fullImage: image6,
+        description: ""
+    },{
+        id: 7,
+        name: "GO! GO!",
+        taste: "Ананас, малина",
+        price: '900.000',
+        previewImage: image7,
+        fullImage: image7,
+        description: ""
+    },{
+        id: 8,
+        name: "Good Lemon",
+        taste: "Лимон",
+        price: '900.000',
+        previewImage: image8,
+        fullImage: image8,
+        description: ""
+    },{
+        id: 9,
+        name: "Jah Grapefruit",
+        taste: "Грейпфрут",
+        price: '900.000',
+        previewImage: image9,
+        fullImage: image9,
+        description: ""
+    },{
+        id: 10,
+        name: "Duebreck jagermeister",
+        taste: "Ориентал на травах",
+        price: '900.000',
+        previewImage: image10,
+        fullImage: image10,
+        description: ""
+    },{
+        id: 11,
+        name: "Lastochka",
+        taste: "Сирень, крыжовник",
+        price: '900.000',
+        previewImage: image11,
+        fullImage: image11,
+        description: ""
+    },{
+        id: 12,
+        name: "Нежность",
+        taste: "Обвивающая любовь",
+        price: '900.000',
+        previewImage: image12,
+        fullImage: image12,
+        description: ""
+    },{
+        id: 13,
+        name: "Queen Anne's Revenge",
+        taste: "Индийский берли",
+        price: '900.000',
+        previewImage: image13,
+        fullImage: image13,
+        description: ""
+    },{
+        id: 14,
+        name: "Точка G",
+        taste: "Клубничный малибу",
+        price: '900.000',
+        previewImage: image14,
+        fullImage: image14,
+        description: ""
+    },{
+        id: 15,
+        name: "Turbo",
+        taste: "Апельсиновая шипучка",
+        price: '900.000',
+        previewImage: image15,
+        fullImage: image15,
+        description: ""
+    },
 ];
 
 export default satyr;
--- src/data/tobacco/starline.js
+++ src/data/tobacco/starline.js
@@ -1,17 +1,151 @@
+import image1 from "../../img/starline/ananas.png";
+import image2 from "../../img/starline/belgianwaffles.png";
+import image3 from "../../img/starline/berrypopcorn.png";
+import image4 from "../../img/starline/cherry.png";
+import image5 from "../../img/starline/coconutmilk.png";
+import image6 from "../../img/starline/cranberry.png";
+import image7 from "../../img/starline/energy.png";
+import image8 from "../../img/starline/kiwi.png";
+import image9 from "../../img/starline/lemon.png";
+import image10 from "../../img/starline/lemongrass.png";
+import image11 from "../../img/starline/melon.png";
+import image12 from "../../img/starline/pear.png";
+import image13 from "../../img/starline/pina.png";
+import image14 from "../../img/starline/raspberry.png";
+import image15 from "../../img/starline/raspberrywaffles.png";
+import image16 from "../../img/starline/strawberry.png";
+import mh_alova from "../../img/musthave/mh_alova.jpeg";
+import mh_alova1 from "../../img/musthave/mh_alova1.jpeg";
+
 const starline = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Ананас",
+        taste: "Ананас",
+        price: '500.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
+        name: "Бельгийские вафли",
+        taste: "Бельгийские вафли",
+        price: '500.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Ягодный попкорн",
+        taste: "Ягодный попкорн",
+        price: '500.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 4,
+        name: "Вишня",
+        taste: "Вишня",
+        price: '500.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Кокосовое молоко",
+        taste: "Кокосовое молоко",
+        price: '500.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
+    },{
+        id: 6,
+        name: "Клюква",
+        taste: "Клюква",
+        price: '500.000',
+        previewImage: image6,
+        fullImage: image6,
+        description: ""
+    },{
+        id: 7,
+        name: "Энергетик",
+        taste: "Энергетик",
+        price: '500.000',
+        previewImage: image7,
+        fullImage: image7,
+        description: ""
+    },{
+        id: 8,
+        name: "Киви",
+        taste: "Киви",
+        price: '500.000',
+        previewImage: image8,
+        fullImage: image8,
+        description: ""
+    },{
+        id: 9,
+        name: "Лимон",
+        taste: "Лимон",
+        price: '500.000',
+        previewImage: image9,
+        fullImage: image9,
+        description: ""
+    },{
+        id: 10,
+        name: "Лимонграсс",
+        taste: "Лимонграсс",
+        price: '500.000',
+        previewImage: image10,
+        fullImage: image10,
+        description: ""
+    },{
+        id: 11,
+        name: "Дыня",
+        taste: "Дыня",
+        price: '500.000',
+        previewImage: image11,
+        fullImage: image11,
+        description: ""
+    },{
+        id: 12,
+        name: "Груша",
+        taste: "Груша",
+        price: '500.000',
+        previewImage: image12,
+        fullImage: image12,
+        description: ""
+    },{
+        id: 13,
+        name: "Пина Колада",
+        taste: "Пина Колада",
+        price: '500.000',
+        previewImage: image13,
+        fullImage: image13,
+        description: ""
+    },{
+        id: 14,
+        name: "Малина",
+        taste: "Малина",
+        price: '500.000',
+        previewImage: image14,
+        fullImage: image14,
+        description: ""
+    },{
+        id: 15,
+        name: "Малиновые вафли",
+        taste: "Малиновые вафли",
+        price: '500.000',
+        previewImage: image15,
+        fullImage: image15,
+        description: ""
+    },{
+        id: 16,
+        name: "Земляника",
+        taste: "Земляника",
+        price: '500.000',
+        previewImage: image16,
+        fullImage: image16,
+        description: ""
     }
 ];
 
