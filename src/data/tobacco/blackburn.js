f66e03ddb5692436f98d8fc776669beecf6b619f
MaxbIch <shadow.maximka@gmail.com>
2026-04-17 18:13:34 +0700

    add checkout form

--- src/data/tobacco/blackburn.js
+++ src/data/tobacco/blackburn.js
@@ -1,17 +1,50 @@
+import image1 from "../../img/blackburn/bb_ananas.jpeg";
+import image2 from "../../img/blackburn/bb_barb.jpeg";
+import image3 from "../../img/blackburn/bb_lemon.jpeg";
+import image4 from "../../img/blackburn/bb_rasp.jpeg";
+import image5 from "../../img/blackburn/bb_yogurt.jpeg";
+
 const blackburn = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Ananas Shock",
+        taste: "Ананасовый шок",
+        price: '600.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
+        name: "Barberry Shock",
+        taste: "Кислый барбарис",
+        price: '600.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Lemon Shock",
+        taste: "Кислый лимон",
+        price: '600.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 3,
+        name: "Raspberry Shock",
+        taste: "Кислая малина",
+        price: '600.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Epic Yogurt",
+        taste: "Черничный йогурт",
+        price: '600.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
     }
 ];
 
