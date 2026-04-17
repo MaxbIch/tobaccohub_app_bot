2c6705dbe00db6910fe3fe37790d5b08ba91f1ff
MaxbIch <shadow.maximka@gmail.com>
2026-04-17 16:44:02 +0700

    add checkout form

--- src/data/tobacco/endorfin.js
+++ src/data/tobacco/endorfin.js
@@ -1,18 +1,152 @@
+import image1 from "../../img/endorphin/ananas.png";
+import image2 from "../../img/endorphin/blackberry.png";
+import image3 from "../../img/endorphin/blackcurrant.png";
+import image4 from "../../img/endorphin/blueberry.png";
+import image5 from "../../img/endorphin/cacao.png";
+import image6 from "../../img/endorphin/cranberry.png";
+import image7 from "../../img/endorphin/darkpassion.png";
+import image8 from "../../img/endorphin/granate.png";
+import image9 from "../../img/endorphin/grapefruit.png";
+import image10 from "../../img/endorphin/lemon.png";
+import image11 from "../../img/endorphin/mango.png";
+import image12 from "../../img/endorphin/orange.png";
+import image13 from "../../img/endorphin/peach.png";
+import image14 from "../../img/endorphin/pear.png";
+import image15 from "../../img/endorphin/quince.png";
+import image16 from "../../img/endorphin/raspberry.png";
+
+
 const endorfin = [
     {
         id: 1,
-        name: "Supernova",
-        price: 1800,
-        image: "https://via.placeholder.com/300",
-        description: "Мощный холодящий вкус"
-    },
-    {
+        name: "Pineapple",
+        taste: "Ананас",
+        price: '500.000',
+        previewImage: image1,
+        fullImage: image1,
+        description: ""
+    },{
         id: 2,
-        name: "Bananapapa",
-        price: 1750,
-        image: "https://via.placeholder.com/300",
-        description: "Банан с кремом"
+        name: "Blackberry",
+        taste: "Ежевика",
+        price: '500.000',
+        previewImage: image2,
+        fullImage: image2,
+        description: ""
+    },{
+        id: 3,
+        name: "Blackcurrant",
+        taste: "Темный смородина",
+        price: '500.000',
+        previewImage: image3,
+        fullImage: image3,
+        description: ""
+    },{
+        id: 4,
+        name: "Blueberry",
+        taste: "Голубика",
+        price: '500.000',
+        previewImage: image4,
+        fullImage: image4,
+        description: ""
+    },{
+        id: 5,
+        name: "Cacao",
+        taste: "Какао",
+        price: '500.000',
+        previewImage: image5,
+        fullImage: image5,
+        description: ""
+    },{
+        id: 6,
+        name: "Cranberry",
+        taste: "Клюква",
+        price: '500.000',
+        previewImage: image6,
+        fullImage: image6,
+        description: ""
+    },{
+        id: 7,
+        name: "Dark passion",
+        taste: "Маракуйя",
+        price: '500.000',
+        previewImage: image7,
+        fullImage: image7,
+        description: ""
+    },{
+        id: 8,
+        name: "Pomegranate",
+        taste: "Гранта",
+        price: '500.000',
+        previewImage: image8,
+        fullImage: image8,
+        description: ""
+    },{
+        id: 9,
+        name: "Grapefruit",
+        taste: "Грейпфрут",
+        price: '500.000',
+        previewImage: image9,
+        fullImage: image9,
+        description: ""
+    },{
+        id: 10,
+        name: "Lemon",
+        taste: "Лимон",
+        price: '500.000',
+        previewImage: image10,
+        fullImage: image10,
+        description: ""
+    },{
+        id: 11,
+        name: "Mango",
+        taste: "Манго",
+        price: '500.000',
+        previewImage: image11,
+        fullImage: image11,
+        description: ""
+    },{
+        id: 12,
+        name: "Orange",
+        taste: "Апельсин",
+        price: '500.000',
+        previewImage: image12,
+        fullImage: image12,
+        description: ""
+    },{
+        id: 13,
+        name: "Peach",
+        taste: "Персик",
+        price: '500.000',
+        previewImage: image13,
+        fullImage: image13,
+        description: ""
+    },{
+        id: 14,
+        name: "Pear",
+        taste: "Груша",
+        price: '500.000',
+        previewImage: image14,
+        fullImage: image14,
+        description: ""
+    },{
+        id: 15,
+        name: "Quince",
+        taste: "Айва",
+        price: '500.000',
+        previewImage: image15,
+        fullImage: image15,
+        description: ""
+    },{
+        id: 16,
+        name: "Raspberry",
+        taste: "Малина",
+        price: '500.000',
+        previewImage: image16,
+        fullImage: image16,
+        description: ""
     }
+
 ];
 
 export default endorfin;
